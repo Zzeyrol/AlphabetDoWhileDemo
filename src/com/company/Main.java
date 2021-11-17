2e230846b7b48c0563ac44855f0d830d286d0a25
Zzeyrol <alex_good2510@mail.ru>
2021-11-17 19:19:04 +0300

    1

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char ch = 'Я';
+        do {
+            System.out.println(ch);
+            ch--;
+        } while (ch >= 'A');
+    }
+}
